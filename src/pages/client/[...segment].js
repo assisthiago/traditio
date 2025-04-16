0fb902095dd2d430ab7df1291c88c2f7febff16e
Thiago Assis <thiago.medassis@gmail.com>
2025-04-16 16:16:55 -0300

    Client active

---
+++ src/pages/client/[...segment].js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+
+import Layout from "@/components/Layout";
+import { useRouter } from "next/router";
+import { PersonFillCheck, PersonFillExclamation } from "react-bootstrap-icons";
+
+export default function Client() {
+  const logged = true;
+
+  const router = useRouter();
+  const { segment } = router.query;
+  const [_, uid, token] = segment || [];
+
+  return (
+    <Layout logged={logged} currentPage="client">
+      <Container fluid className="pt-5 mt-5">
+        <Row className="justify-content-center">
+          <Col xs={12} className="text-center">
+            <PersonFillExclamation size={50} className="text-primary" />
+            <h6 className="mt-2 mb-5">Ativar minha conta!</h6>
+            <Button size="md" className="py-2 px-5 shadow-lg">
+              <PersonFillCheck size={50} />
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </Layout>
+  )
+}
